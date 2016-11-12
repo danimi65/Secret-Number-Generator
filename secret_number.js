535bf3c421195d583b630b7c065e603a9ffde8ae
Danika Harada <danikaharada@Danikas-MacBook-Air.local>
2016-11-12 12:33:35 -1000

    completed

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,29 @@
 'use strict';
 module.exports = function() {
+var theSecretNumber = Math.random() * 1000000;
+return function generateSecretNumber(){
+  return theSecretNumber;
+
+ 
+
+};
+
   
 };
+
+//======================================================
+
+/* const secret = require('secret_number.js') //if you were importing it into another file
+
+
+var myNum = secret(); // generates random #, returns: function mysecretNumber
+
+myNum(); //123
+myNum(); //123
+
+var myNum2 = secret();
+
+myNum2(); //567
+myNum2(); //567
+
+myNum1(); //123 */
